2b92756735dbde8cb76a0fbde893c63992ef8d73
Jawad Makhoul <jawad.makhol@gmail.com>
2024-11-23 23:21:16 +0200

    Final version

--- recusrion.py
+++ recusrion.py
@@ -1,26 +1,24 @@
 def fibonacci(n):
     if n <= 0:
-        return 0  # Base case: F(0) = 0
+        return 0  
     elif n == 1:
-        return 1  # Base case: F(1) = 1
+        return 1  
     else:
-        return fibonacci(n - 1) + fibonacci(n - 2)  # Recursive call
-
-# Example Input
-print(fibonacci(6))  # Expected Output: 8
+        return fibonacci(n - 1) + fibonacci(n - 2)  # main line in the code ( how the rec works )
 
 
+print(fibonacci(6))  
 
-def find_subsets(arr):
-    subsets = []  # Initialize an empty list to store subsets
-    subsets.append([])  # Add the empty subset
+#####################################################################
+
+def findSubsets(arr):
+    subsets = []  # list to store subsets
+    subsets.append([])  
 
     for i in arr:
-        # Add each element to the existing subsets
         new_subsets = [subset + [i] for subset in subsets]
-        subsets.extend(new_subsets)  # Extend the subsets with the new subsets
+        subsets.extend(new_subsets)  
+    return subsets 
 
-    return subsets  # Return the list of subsets
 
-# Example usage
-print(find_subsets([1, 2]))
+print(findSubsets([1, 2]))
